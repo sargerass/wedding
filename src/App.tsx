0ec38569b32415b6744db1eb0c2a9d09527fa0ad
Guillermo <gdavidas3@gmail.com>
2021-08-07 15:53:28 -0500

    connection with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import HomePage from './pages/home/home';
-function App() {
-  return (
-    <div className="App">
-      <HomePage/>
-    </div>
-  );
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./pages/home/home";
+
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this._setup();
+  }
+  private _setup(): void {
+    
+  }
+  render() {
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
