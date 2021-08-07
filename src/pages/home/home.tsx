0e094a446b6fb6ac74d3c66dda7587aeb1fd28d4
Guillermo <gdavidas3@gmail.com>
2021-08-07 14:36:37 -0500

    fix home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -6,18 +6,44 @@ interface IProps {
   onFinish?: Function;
 }
 interface IState {
-  showHeart: boolean;
-  showHelp: boolean;
+
 }
 class HomePage extends React.Component<IProps, IState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+    };
+    this._setup();
+  }
+  private _setup(): void {
+    this._validateDocument = this._validateDocument.bind(this);
+  }
+  private _validateDocument(e:any): void {
+    e.preventDefault();
+    console.log('e', e);
+    const form = e.target;
+    const document = form.document.value;    
+  }
   render() {
     return (
       <div className="page-home">
         <HeartsComponent/>
         <div className="page-home__content">
-          <div className="page-home__title">
-            Cristina y Guillermo
+          <div>
+            <div className="page-home__title">
+              Bievenido a nuestra boda
+            </div>
+            <div>
+              Cristina <br /> y  <br /> Guillermo
+            </div>
+            <form onSubmit={this._validateDocument}>
+              <input name="document" type="tel" placeholder="Documento" required/>
+              <br />
+              <button>Ingresar</button>
+            </form>
           </div>
+          
         </div>
       </div>
     )
