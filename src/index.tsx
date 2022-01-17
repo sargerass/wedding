8c20a08789c4e1b3123f787294cf2f69138ad64b
Guillermo <gdavidas3@gmail.com>
2022-01-17 09:01:35 -0500

    mjoera

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,17 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+if (navigator.serviceWorker.controller) {
+  console.log('Service worker already registered.')
+} else {
+  navigator.serviceWorker.register('service-worker.js', {
+    scope: './'
+  }).then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
